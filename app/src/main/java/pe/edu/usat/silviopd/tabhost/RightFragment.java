968bcf028995172df3016ca1dae1c3948949de4a
silviopd <silviopd01@gmail.com>
2018-10-18 17:09:22 -0500

    Initial commit

---
+++ app/src/main/java/pe/edu/usat/silviopd/tabhost/RightFragment.java
@@ -0,0 +1,23 @@
+package pe.edu.usat.silviopd.tabhost;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class RightFragment extends Fragment {
+
+    public RightFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_right, container, false);
+    }
+}
